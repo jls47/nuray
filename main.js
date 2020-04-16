d0ebfab169aaf741695ef59ccbeb9125a70143bb
jls47 <lukeschaefer@gmail.com>
2020-04-16 12:58:20 -0700

    embed

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ document.addEventListener("click", function(event){
 	var targeted = event.target.id;
 	console.log(targeted);
 	if(targeted == "math"){
-		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/watch?v=mlwBm4Kp_gM" id = "addedVid"></iframe>';
+		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/embed?v=mlwBm4Kp_gM" id = "addedVid"></iframe>';
 	}else if (targeted == "science"){
-		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/watch?v=3TBF_p-Ytp8" id = "addedVid"></iframe>';
+		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/embed?v=3TBF_p-Ytp8" id = "addedVid"></iframe>';
 	}else if (targeted == "english"){
-		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/watch?v=uE7Uz8-MRw8" id = "addedVid"></iframe>';
+		document.getElementById("videoBody").innerHTML += '<iframe width="420" height="315" src="https://www.youtube.com/embed?v=uE7Uz8-MRw8" id = "addedVid"></iframe>';
 	}
 
 })
